8cfb28497436d521f54d489543e90fc033031a73
Tim Lucas <t@toolmantim.com>
2016-12-11 17:51:13 +1100

    Upload first

--- .buildkite/steps/1-merry-xmas.bash
+++ .buildkite/steps/1-merry-xmas.bash
@@ -6,6 +6,8 @@ function inline_image {
   printf '\033]1338;url='"$1"';alt='"$2"'\a\n'
 }
 
+buildkite-agent artifact upload secret-santa.gif
+
 echo "+++ :christmas_tree: <3"
 
 inline_image 'artifact://secret-santa.gif' 'Buildkite Secret Santa 2016'
