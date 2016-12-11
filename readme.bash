2ac0798c57c663177018faee2d3a4dd91717f7db
Tim Lucas <t@toolmantim.com>
2016-12-11 23:39:40 +1100

    Lining it up

--- readme.bash
+++ readme.bash
@@ -13,10 +13,10 @@ inline_image 'artifact://secret-santa.gif' 'Buildkite Secret Santa 2016'
 ./xmas-tree.bash
 
 echo
-echo -e "                 \033[31mWelcome to the Buildkite Secret Santa\033[0m 👋"
+echo -e "                \033[31mWelcome to the Buildkite Secret Santa\033[0m 👋"
 echo
-echo -e "            Tell Santa your wishes. And when everyone is done,"
-echo -e "               Santa’s magical unicorns will let each of us"
-echo -e "                know by email who we received, and a hint"
-echo -e "                  for what they’d \033[31m<3\033[0m for Christmas 🦄"
+echo -e "           Tell Santa your wishes. And when everyone is done,"
+echo -e "              Santa’s magical unicorns will let each of us"
+echo -e "               know by email who we received, and a hint"
+echo -e "                 for what they’d \033[31m<3\033[0m for Christmas 🦄"
 echo
