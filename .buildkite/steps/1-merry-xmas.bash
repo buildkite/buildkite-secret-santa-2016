f79496e439e538a1cb6ec58d4734c0ab7f9a172a
Tim Lucas <t@toolmantim.com>
2016-12-11 16:56:13 +1100

    No bash fancy

--- .buildkite/steps/1-merry-xmas.bash
+++ .buildkite/steps/1-merry-xmas.bash
@@ -1,49 +1,5 @@
 #!/bin/bash
 
-LINES=$(tput lines)
-COLUMNS=$(tput cols)
-
-declare -A snowflakes
-declare -A lastflakes
-
-clear
-
-function move_flake() {
-  i="$1"
-
-  if [ "${snowflakes[$i]}" = "" ] || [ "${snowflakes[$i]}" = "$LINES" ]; then
-    snowflakes[$i]=0
-  else
-    if [ "${lastflakes[$i]}" != "" ]; then
-      printf "\033[%s;%sH \033[1;1H " ${lastflakes[$i]} $i
-    fi
-  fi
-
-  printf "\033[%s;%sH❄️\033[1;1H" ${snowflakes[$i]} $i
-
-  lastflakes[$i]=${snowflakes[$i]}
-  snowflakes[$i]=$((${snowflakes[$i]}+1))
-}
+set -eu
 
 echo "+++ :christmas_tree: <3"
-
-j=0
-
-while :
-do
-  i=$(($RANDOM % $COLUMNS))
-  j=$j+1
-
-  move_flake $i
-
-  for x in "${!lastflakes[@]}"
-  do
-      move_flake "$x"
-  done
-
-  sleep 0.1
-
-  if [[ $j -gt 100 ]]; then
-    break
-  fi
-done
