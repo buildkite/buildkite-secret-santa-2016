81f187dd05d481d8509980fa8e8ea52d9c52e6f2
Tim Lucas <t@toolmantim.com>
2016-12-11 17:41:56 +1100

    Bling

--- .buildkite/steps/1-merry-xmas.bash
+++ .buildkite/steps/1-merry-xmas.bash
@@ -2,4 +2,10 @@
 
 set -eu
 
+function inline_image {
+  printf '\033]1338;url='"$1"';alt='"$2"'\a\n'
+}
+
 echo "+++ :christmas_tree: <3"
+
+inline_image 'artifact://secret-santa.gif' 'Buildkite Secret Santa 2016'
